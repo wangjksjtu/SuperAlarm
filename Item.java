b94a9a0a35e5ebd78cad14f17ee2c6d7fb455958
linboyang <1095137526@qq.com>
2016-10-23 18:13:31 +0800

    Update Item.java
    加入了一个新的私有成员变量，classTitle ，对应app上面的类，比如食物类和运动类，每个item 自带这个成员，不必再重新创建类。
    增加写入功能，创建classTitle文件夹，将每个item的属性写入到文件名为title的txt文件中

--- Item.java
+++ Item.java
@@ -1,24 +1,31 @@
-package basic;
-
+package item;
+import java.io.*;
+import java.io.IOException;
+/**date 10.23 
+ * @author yanglinbo
+ */
 /**
  * date:2016-10-02
  * @author wangjksjtu
  */
 
 public class Item {
+	private String classTitle;
     private String title;
     private String deadline;
     private int importance; 
     private String content;
-    public Item(String t, String d, int i, String c) {
-        title = t; deadline = d; importance = i; content = c;
+    public Item(String cl,String t, String d, int i, String c) {
+       classTitle=cl; title = t; deadline = d; importance = i; content = c;
     }
-    public Item(String t, String d, int i) {this(t,d,i,"");}
-    public Item(){this("","",1,"");}
+    public Item(String cl,String t, String d, int i) {this(cl,t,d,i,"");}
+    public Item(){this("","","",1,"");}
+    String getClasTitle(){return classTitle;}
     String getTitle() {return title;}
     String getDeadline() {return deadline;}
     int getImportance() {return importance;}
     String getContent() {return content;}
+    void setClassTitle(String cl){classTitle =cl; }
     void setTitle(String t) {title = t;}
     void setDeadline(String d) {deadline = d;}
     void setImportance(int i) {importance = i;}
@@ -26,13 +33,34 @@ public class Item {
     boolean equals(Item it) {
         if (this == it) return true;
         if (it == null) return false;
-        return title.equals(it.title) && deadline.equals(it.deadline) && (importance == it.importance) 
+        return classTitle.equals(it.classTitle)&&title.equals(it.title) && deadline.equals(it.deadline) && (importance == it.importance) 
                 && content.equals(it.content);
     }
     void copy(Item it) { //After test, I will delete the public
+    	this.classTitle=it.classTitle;
         this.content = it.content;
         this.deadline = it.deadline;
         this.importance = it.importance;
         this.title = it.title;
+    }
+    
+    //xieRu
+    public void write ()throws IOException {
+    String path;
+    String fileName;
+    path="D:/SuperAlarm"+classTitle;
+    fileName=path+"/"+title+".txt";
+    File filePath=new File(path);
+    if (!filePath.exists()){filePath.mkdir();}
+    FileWriter fw =new FileWriter(fileName,true);
+    fw.write(classTitle+"\r\n");
+    fw.write(title+"\r\n");
+    fw.write(deadline+"\r\n");
+    char simportance;
+    simportance=(char)(importance+48);   
+    fw.write(simportance+"\r\n");
+    fw.write(content+"\r\n");
+    fw.close();
+    
     }
 }
