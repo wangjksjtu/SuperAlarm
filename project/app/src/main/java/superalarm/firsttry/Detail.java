ddb567f5cce7474cee0071d20cbd1380c4148e8f
somecat1996 <369150573@qq.com>
2016-11-17 16:34:15 +0800

    use listview to show item preview

--- project/app/src/main/java/superalarm/firsttry/Detail.java
+++ project/app/src/main/java/superalarm/firsttry/Detail.java
@@ -9,14 +9,25 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.content.Intent;
+import android.widget.TextView;
 
 public class Detail extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("string_key");
+
+        TextView title = (TextView)findViewById(R.id.textView2);
+        TextView info = (TextView)findViewById(R.id.textView4);
+
+        title.setText(data);
+        info.setText(data+data);
 
         ImageButton btn_revert = (ImageButton)findViewById(R.id.imageButton_revert);
         btn_revert.setOnClickListener(new Button.OnClickListener() {
--- project/app/src/main/java/superalarm/firsttry/MainActivity.java
+++ project/app/src/main/java/superalarm/firsttry/MainActivity.java
@@ -1,5 +1,6 @@
 package superalarm.firsttry;
 
+import android.app.ListActivity;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -10,11 +11,22 @@ import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextClock;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +35,39 @@ public class MainActivity extends AppCompatActivity {
     private TextClock dTextClock;
     private boolean have_login;
     private int mId = 0;
+    private ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R.id.list);
+        ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String,Object>>();/*在数组中存放数据*/
+        for (int i = 0;i<=20;i++){
+            HashMap<String,Object> map = new HashMap<String,Object>();
+            map.put("ItemImage",R.drawable.btn_add_press);
+            map.put("ItemTitle","第"+i+"行");
+            map.put("ItemText","这是第"+i+"行");
+            listItem.add(map);
+        }
+
+        SimpleAdapter mSimpleAdaptr = new SimpleAdapter(this,listItem,R.layout.listitem,
+                new String[]{"ItemImage","ItemTitle","ItemText"},new int[]{R.id.ItemImage,R.id.ItemTitle,R.id.ItemText});
+        lv.setAdapter(mSimpleAdaptr);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                   long arg3) {
+                Intent intent = new Intent(MainActivity.this,Detail.class);
+                Bundle bundle = new Bundle();
+                intent.putExtra("string_key","0"+arg2);
+                startActivity(intent);
+            }
+        });
+
         instance = this;
 
         hTextClock = (TextClock)findViewById(R.id.textClock3);
