899de138e2d2638c910f2f73bb8e3a225f180da7
wangjksjtu <249446879@qq.com>
2016-11-21 23:58:06 +0800

    Successfully finish http request get, delete, put, post

--- project/app/src/main/java/superalarm/firsttry/JsonParser.java
+++ project/app/src/main/java/superalarm/firsttry/JsonParser.java
@@ -10,30 +10,43 @@ import org.json.JSONObject;
 
 public class JsonParser {
     private String finalJson;
-    JsonParser(String finalJsonJ){
-        finalJsonJ = finalJsonJ;
+    JsonParser(String string){
+        finalJson = string;
     };
     String JsonParserItem () throws JSONException {
         JSONObject parentObject = new JSONObject(finalJson);
         JSONArray parentArray = parentObject.getJSONArray("results");
 
         StringBuffer finalBufferData = new StringBuffer();
-        for (int i = 0; i<parentArray.length(); i++) {
+        for (int i = 0; i < parentArray.length(); i++) {
             JSONObject finalObject = parentArray.getJSONObject(i);
-
             String title = finalObject.getString("title");
-            String deadline = finalObject.getString("title");
+            String deadline = finalObject.getString("deadline");
             String module = finalObject.getString("module");
             String content = finalObject.getString("content");
-            finalBufferData.append(title + "|" + deadline + "|" + module
-            + "|" + content );
+            finalBufferData.append(title + "|||" + deadline + "|||" + module
+            + "|||" + content +"\n");
         }
         return finalBufferData.toString();
     }
 
-    /*
-    String JsonparserItemOfGroup
-    } */
+    String JsonparserItemOfGroup() throws JSONException {
+        JSONObject parentObject = new JSONObject(finalJson);
+        JSONArray parentArray = parentObject.getJSONArray("results");
+        StringBuffer finalBufferData = new StringBuffer();
+        for (int i = 0; i < parentArray.length(); i++) {
+            JSONObject finalObject = parentArray.getJSONObject(i);
+            String groupId = finalObject.getString("id");
+            String groupname = finalObject.getString("groupname");
+            String title = finalObject.getString("title");
+            String deadline = finalObject.getString("deadline");
+            String module = finalObject.getString("module");
+            String content = finalObject.getString("content");
+            finalBufferData.append(title + "|||" + deadline + "|||" + module
+                    + "|||" + content +"\n");
+        }
+        return finalBufferData.toString();
+    }
 
     /*
     String JsonParserGroup
