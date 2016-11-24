bd5465ebbed85a0ae91ac690df953c908645fcb5
somecat1996 <369150573@qq.com>
2016-11-24 15:31:59 +0800

    实现事项的修改与删除

--- project/app/src/main/java/superalarm/firsttry/event_start_activity.java
+++ project/app/src/main/java/superalarm/firsttry/event_start_activity.java
@@ -15,6 +15,9 @@ import android.widget.RatingBar;
 import android.widget.RatingBar.OnRatingBarChangeListener;
 import android.widget.TextView;
 
+import basic_class.Item;
+import basic_class.ItemManager;
+
 
 public class event_start_activity extends AppCompatActivity {
 
@@ -25,11 +28,14 @@ public class event_start_activity extends AppCompatActivity {
     private TextView typeTitle;
     private RadioGroup annoyMode;
     private RadioButton annoySingle, annoyFrequent;
+    private boolean key;
+    private int item_num = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        boolean key;
+        ItemManager itemManager = new ItemManager();
+        Item item = new Item();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_starter);
@@ -37,16 +43,17 @@ public class event_start_activity extends AppCompatActivity {
 
         //intent接收事项类型
         String eventType;
-        Intent intent = getIntent();
-        key = intent.getBooleanExtra("key",false);
+        Intent intent_receive = getIntent();
+        key = intent_receive.getBooleanExtra("key",false);
         if (!key) {
-            eventType = intent.getStringExtra("typeConvey");
+            eventType = intent_receive.getStringExtra("typeConvey");
         }
         else {
-            eventType = intent.getStringExtra("value");
+            item_num = intent_receive.getIntExtra("value",-1);
+            itemManager.read(event_start_activity.this);
+            item = itemManager.itemArr.get(item_num);
+            eventType = item.getClassTitle();
         }
-        Intent intent_receive = getIntent();
-        eventType = intent_receive.getStringExtra("typeConvey");
         typeTitle = (TextView) findViewById(R.id.textTitleMid);
         typeTitle.setText(eventType);
 
@@ -62,10 +69,23 @@ public class event_start_activity extends AppCompatActivity {
             }
         });
 
+        if (key){
+            textEventName.setText(item.getTitle());
+            textEventDetails.setText(item.getContent());
+        }
+
         //单选按钮的定义
         annoyMode = (RadioGroup) findViewById(R.id.RadioGroupAnnoy);
         annoySingle = (RadioButton) findViewById(R.id.RadioButtonSingle);
         annoyFrequent = (RadioButton) findViewById(R.id.RadioButtonFrequency);
+
+        //设置单选按钮默认值
+        if (key){
+            String deadline = item.getDeadline();
+            int length = deadline.length();
+            if (length > 6) annoySingle.setChecked(true);
+            else annoyFrequent.setChecked(true);
+        }
 
         //OK按钮和CANCEL按钮的链接
         btOkEvent = (Button)findViewById(R.id.buttonOkEvent);
@@ -74,6 +94,8 @@ public class event_start_activity extends AppCompatActivity {
             public void onClick(View v) {
                 if ( annoyMode.getCheckedRadioButtonId() == annoySingle.getId() ) {
                     Intent intent_send = new Intent(event_start_activity.this, time_set_activity.class);
+                    intent_send.putExtra("key",key);
+                    intent_send.putExtra("num",item_num);
                     intent_send.putExtra("eventName",textEventName.getText().toString());
                     intent_send.putExtra("eventDetail",textEventDetails.getText().toString());
                     intent_send.putExtra("eventType",typeTitle.getText().toString());
@@ -81,8 +103,14 @@ public class event_start_activity extends AppCompatActivity {
                     startActivity(intent_send);
                 }
                 else if ( annoyMode.getCheckedRadioButtonId() == annoyFrequent.getId() ) {
-                    Intent intent = new Intent(event_start_activity.this, time_frequency_activity.class);
-                    startActivity(intent);
+                    Intent intent_send = new Intent(event_start_activity.this, time_frequency_activity.class);
+                    intent_send.putExtra("key",key);
+                    intent_send.putExtra("num",item_num);
+                    intent_send.putExtra("eventName",textEventName.getText().toString());
+                    intent_send.putExtra("eventDetail",textEventDetails.getText().toString());
+                    intent_send.putExtra("eventType",typeTitle.getText().toString());
+                    intent_send.putExtra("eventRating",ratingBar.getRating());
+                    startActivity(intent_send);
                 }
             }
         });
